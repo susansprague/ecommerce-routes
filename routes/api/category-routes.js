12362a9c1170e04e3ede7207e32f4b0dec74030a
Your Name <you@example.com>
2023-09-06 16:26:12 -0400

    added cateogry routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,19 +15,41 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  }).then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
+    res.json(err);
+  })
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
+    res.json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
+    res.json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
+    res.json(err);
+  })
 });
 
 module.exports = router;
