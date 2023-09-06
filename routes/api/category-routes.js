f0e091950c1e73f8828408a57baa7a624bbb8bcd
Your Name <you@example.com>
2023-09-06 13:13:50 -0400

    cloned started code and added seeds to db

---
+++ routes/api/category-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  Category.findAll({
+    include: [{ model: Product }],
+  }).then(dbCategoryData => res.json(dbCategoryData)).catch(err => {
+    res.json(err);
+  })
+  // be sure to include its associated Products
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
